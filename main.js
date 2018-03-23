1f1014ad27b2472eec083d9b9d7ca29d1e26c16e
luivilhena <luisavilhena@gmail.com>
2018-03-23 12:27:01 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        margin:0,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    })
+})
